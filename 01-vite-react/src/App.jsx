19c92b8e0feda61fcf102def01641dfd4a2d44bd
Tanishq-Mathur35 <tanishqmathur975@gmail.com>
2026-03-21 18:02:09 +0530

    Custom React

--- 01-vite-react/src/App.jsx
+++ 01-vite-react/src/App.jsx
@@ -1,10 +1,12 @@
 import Hello from "./hello.jsx";
 
 function App() {
+    const username = "Tanishq Mathur";
+
     return (
         <>
             <Hello />
-            <h1>I am learning React</h1>
+            <h1>I am learning React - {username}</h1>
             <p>test para</p>
         </>
     );
--- 01-vite-react/src/main.jsx
+++ 01-vite-react/src/main.jsx
@@ -1,9 +1,37 @@
-import { StrictMode } from "react";
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <App />
-    </StrictMode>,
+function MyApp() {
+    return (
+        <div>
+            <h1>Custom App</h1>
+        </div>
+    );
+}
+
+// const ReactElement = {
+//     type: "a",
+//     props: {
+//         href: "https://google.com",
+//         target: "_blank",
+//     },
+//     childern: "Click me to visit google",
+// };
+
+const anotherElement = (
+    <a href="https://google.com" target="_blank">
+        Visit Google
+    </a>
 );
+
+const anotherUser = "learning React";
+
+const ReactElement = React.createElement(
+    "a",
+    { href: "https://google.com", target: "_blank" },
+    "Click me to visit google",
+    anotherUser,
+);
+
+createRoot(document.getElementById("root")).render(anotherElement);
