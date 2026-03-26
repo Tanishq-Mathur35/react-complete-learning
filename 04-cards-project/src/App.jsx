227f9c1029d80df8d6bcc66d8aa929acde4b9eb1
Tanishq-Mathur35 <tanishqmathur975@gmail.com>
2026-03-26 21:34:35 +0530

    Update cards project

--- 04-cards-project/src/App.jsx
+++ 04-cards-project/src/App.jsx
@@ -8,10 +8,11 @@ const App = () => {
             datePosted: "5 days ago",
             post: "Frontend Engineer",
             tag1: "Full Time",
-            tag2: "Junior Level",
+            tag2: "Junior",
             pay: "$65/hour",
-            location: "Menlo Park, USA",
+            location: "Menlo Park",
         },
+
         {
             brandLogo:
                 "https://static.vecteezy.com/system/resources/previews/014/018/561/non_2x/amazon-logo-on-transparent-background-free-vector.jpg",
@@ -19,117 +20,63 @@ const App = () => {
             datePosted: "2 weeks ago",
             post: "Backend Developer",
             tag1: "Full Time",
-            tag2: "Mid Level",
+            tag2: "Mid",
             pay: "$70/hour",
-            location: "Hyderabad, India",
+            location: "Hyderabad",
         },
+
         {
             brandLogo:
-                "https://substackcdn.com/image/fetch/$s_!G1lk!,f_auto,q_auto:good,fl_progressive:steep/https%3A%2F%2Fsubstack-post-media.s3.amazonaws.com%2Fpublic%2Fimages%2F8ed3d547-94ff-48e1-9f20-8c14a7030a02_2000x2000.jpeg",
-            companyName: "Apple",
-            datePosted: "3 weeks ago",
-            post: "iOS Developer",
-            tag1: "Part Time",
-            tag2: "Senior Level",
-            pay: "$90/hour",
-            location: "Cupertino, USA",
-        },
-        {
-            brandLogo:
-                "https://images.ctfassets.net/4cd45et68cgf/Rx83JoRDMkYNlMC9MKzcB/2b14d5a59fc3937afd3f03191e19502d/Netflix-Symbol.png?w=700&h=456",
-            companyName: "Netflix",
-            datePosted: "10 days ago",
-            post: "Machine Learning Engineer",
-            tag1: "Full Time",
-            tag2: "Senior Level",
-            pay: "$110/hour",
-            location: "Los Gatos, USA",
-        },
-        {
-            brandLogo:
-                "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/1200px-Google_%22G%22_logo.svg.png",
+                "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/512px-Google_2015_logo.svg.png",
             companyName: "Google",
             datePosted: "1 week ago",
-            post: "Cloud Solutions Architect",
+            post: "Cloud Engineer",
             tag1: "Full Time",
-            tag2: "Mid Level",
-            pay: "$85/hour",
-            location: "Bangalore, India",
+            tag2: "Senior",
+            pay: "$90/hour",
+            location: "Bangalore",
         },
+
         {
             brandLogo:
-                "https://download.logo.wine/logo/Microsoft_Store/Microsoft_Store-Logo.wine.png",
-            companyName: "Microsoft",
-            datePosted: "4 weeks ago",
-            post: "Data Scientist",
-            tag1: "Full Time",
-            tag2: "Junior Level",
-            pay: "$75/hour",
-            location: "Redmond, USA",
-        },
-        {
-            brandLogo:
-                "https://blog.logomaster.ai/hs-fs/hubfs/ibm-logo-1967.jpg?width=672&height=454&name=ibm-logo-1967.jpg",
-            companyName: "IBM",
-            datePosted: "2 days ago",
-            post: "AI Research Engineer",
-            tag1: "Full Time",
-            tag2: "Senior Level",
-            pay: "$95/hour",
-            location: "New York, USA",
-        },
-        {
-            brandLogo:
-                "https://www.pngplay.com/wp-content/uploads/13/Tesla-Logo-PNG-HD-Quality.png",
+                "https://1000logos.net/wp-content/uploads/2017/03/Tesla-Logo.png",
             companyName: "Tesla",
-            datePosted: "6 days ago",
+            datePosted: "3 days ago",
             post: "Software Engineer",
             tag1: "Full Time",
-            tag2: "Mid Level",
+            tag2: "Mid",
             pay: "$80/hour",
-            location: "Austin, USA",
+            location: "Austin",
         },
+
         {
             brandLogo:
-                "https://www.nvidia.com/content/dam/en-zz/Solutions/about-nvidia/logo-and-brand/nvidia-og-image-white-bg-1200x630.jpg",
-            companyName: "NVIDIA",
-            datePosted: "3 weeks ago",
-            post: "GPU Programmer",
+                "https://upload.wikimedia.org/wikipedia/commons/4/44/Microsoft_logo.svg",
+            companyName: "Microsoft",
+            datePosted: "4 days ago",
+            post: "Data Scientist",
             tag1: "Full Time",
-            tag2: "Senior Level",
-            pay: "$120/hour",
-            location: "Santa Clara, USA",
-        },
-        {
-            brandLogo:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnOVjTWaheo4E99cgYZ6y14tpsgHlm0VN8Hw&s",
-            companyName: "Oracle",
-            datePosted: "8 days ago",
-            post: "Database Administrator",
-            tag1: "Full Time",
-            tag2: "Mid Level",
-            pay: "$70/hour",
-            location: "Mumbai, India",
+            tag2: "Junior",
+            pay: "$75/hour",
+            location: "Seattle",
         },
     ];
 
     return (
         <div className="parent">
-            {jobOpenings.map(function (elem, idx) {
-                return (
-                    <div key={idx}>
-                        <Card
-                            company={elem.companyName}
-                            datePosted={elem.datePosted}
-                            post={elem.post}
-                            tag1={elem.tag1}
-                            brandLogo={elem.brandLogo}
-                            pay={elem.pay}
-                            tag2={elem.tag2}
-                        />
-                    </div>
-                );
-            })}
+            {jobOpenings.map((job, i) => (
+                <Card
+                    key={i}
+                    company={job.companyName}
+                    datePosted={job.datePosted}
+                    post={job.post}
+                    tag1={job.tag1}
+                    tag2={job.tag2}
+                    pay={job.pay}
+                    brandLogo={job.brandLogo}
+                    location={job.location}
+                />
+            ))}
         </div>
     );
 };
