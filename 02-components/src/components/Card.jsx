4a53f7b87c46cf06d961932f2c587b6caaea79e6
Tanishq-Mathur35 <tanishqmathur975@gmail.com>
2026-03-26 17:53:26 +0530

    Components based architecture

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div>
+            <div className="card">
+                <h1>Tanishq Mathur</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Autem tempore iure porro natus animi deserunt quos, in
+                    quibusdam voluptatibus alias cum quae, qui consectetur,
+                    inventore sed aspernatur velit molestias ullam?
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
