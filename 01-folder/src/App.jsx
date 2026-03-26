802ea05029d5adbb5f2d523e95dbf61b28db8b83
Tanishq-Mathur35 <tanishqmathur975@gmail.com>
2026-03-26 13:18:56 +0530

    Basic folder setup with vite

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <>
+            <h1>Hello</h1>
+            <h2>I am Tanishq Mathur</h2>
+        </>
+    );
+};
+
+export default App;
