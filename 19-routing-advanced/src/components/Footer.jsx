9f5d72ff09ca35a2e21859c106dd37315ae5ec6c
Tanishq-Mathur35 <tanishqmathur975@gmail.com>
2026-03-31 15:15:12 +0530

    React Router

---
+++ 19-routing-advanced/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <div className="py-4 absolute bottom-0 w-full bg-cyan-900 flex items-center justify-center">
+            <h3>This is Footer</h3>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ 19-routing-advanced/src/components/Navigate.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Navigate = () => {
+    const navigate = useNavigate();
+
+    return (
+        <div className="py-2 px-6 bg-cyan-800">
+            <button
+                onClick={() => {
+                    navigate("/");
+                }}
+                className=" font-medium bg-amber-500 px-5 py-2 rounded m-2 cursor-pointer active:scale-95"
+            >
+                Return to Home Page
+            </button>
+
+            <button
+                onClick={() => {
+                    navigate(-1);
+                }}
+                className=" font-medium bg-amber-500 px-5 py-2 rounded m-2 cursor-pointer active:scale-95"
+            >
+                Back
+            </button>
+
+            <button
+                onClick={() => {
+                    navigate(+1);
+                }}
+                className=" font-medium bg-amber-500 px-5 py-2 rounded m-2 cursor-pointer active:scale-95"
+            >
+                Next
+            </button>
+        </div>
+    );
+};
+
+export default Navigate;
